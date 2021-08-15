e13182648353f84796c54c02845aded7ea58dd0b
thiyagutenysen <37877845+thiyagutenysen@users.noreply.github.com>
2021-08-15 15:57:16 +0530

    Add files via upload

---
+++ Scrape_amazon_flipkart_prices/webscrape24.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 11 18:25:58 2019
+
+@author: chella
+"""
+import json
+import sys
+#import SocketServer
+
+#request = json.load(sys.stdin)
+print('hello')
+#response = SocketServer.handle_request(request)
+print("Content-Type: application/json", end="\n\n")
+#json.dump(response, sys.stdout, indent=2)
